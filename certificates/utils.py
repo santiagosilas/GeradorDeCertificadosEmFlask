dfea10c4be765e98e2c30edb57ea777afdcd23f6
Silas Santiago <santiagosilas@outlook.com>
2020-01-11 16:57:03 -0300

    .

--- certificates/utils.py
+++ certificates/utils.py
@@ -3,9 +3,9 @@ from os.path import pardir
 from docx import Document
 from docx.shared import Pt
 
-def create_certificate(template, curso, nomes):
+def create_certificate(template, nome, titulo):
     '''
-    O template deve conter as palavras @nome e @curso
+    O template deve conter as palavras <nome> e <titulo>
     '''
     print(template)
     if os.path.exists(template):
@@ -13,12 +13,12 @@ def create_certificate(template, curso, nomes):
         output_dir = os.path.join(os.path.split(template)[0], 'output')
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-
-        for index, nome in enumerate(nomes):
-            new_file = os.path.join(output_dir, '{0}.docx'.format(index))
-            replace(document, '<curso>', curso.upper())
-            replace(document, '<nome>', nome.upper())
-            document.save(new_file)
+        
+        print(nome, titulo)
+        new_file = os.path.join(output_dir, '{0}.docx'.format(nome))
+        replace(document, '<titulo>', titulo.upper())
+        replace(document, '<nome>', nome.upper())
+        document.save(new_file)
     else:
         print('Template not found!')
     
@@ -33,8 +33,15 @@ def replace(document, search, replace):
                     font.size = Pt(18)
 
 if __name__ == '__main__':
-    path = 'C:\\temp\\template.docx'
-    nomes = ['Fulano de Tal', "Maria Fulana"]
-    curso = 'Curso de Corte e Costura'
-    create_certificate(path, curso, nomes)
+    template = 'E:\\gdrive\\H-References\\H-References-Working\\EP_EXTENSAO\\2017\\PAPEX 2017\\2017-PROPYTHON\\2017-11-24 1 Encontro PyLadies\\declaracao.docx'
+    pessoas = [
+        ['Alice Feitosa Barbosa','PyLadies, Divas e ProPython'],
+        ['Helen Abdala Rocha Ferreira','PyLadies, Divas e ProPython'],
+        ['JOYCE QUINTINO ALVES', 'Chatterbots com Python'],
+        ['JOSÉ WILLIAM VITORINO DE SOUZA', 'Introdução ao Tkinter'],
+        ['IVYSON LUCAS DE LIMA SILVA', 'Introdução ao Kivy'],
+        ['PEDRO MICHAEL DOS SANTOS SOARES', 'Python em 10 minutos']
+        ]
+    for nome, titulo in pessoas:
+        create_certificate(template, nome, titulo)
     print('done')
